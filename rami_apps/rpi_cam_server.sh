adc3ed67563eec58ec29fafcf022482c82b12768
Rami ZAYAT <rami.zayat9@gmail.com>
2024-01-14 17:26:10 +0100

    added new service without motion detection

---
+++ rami_apps/rpi_cam_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/python3.9 -u /home/rpirami/camerpay2python/picamera2/rami_apps/Rami_cam_services.py
--- rami_apps/rpi_gpio_pi.py
+++ rami_apps/rpi_gpio_pi.py
@@ -5,10 +5,17 @@ from paho.mqtt import client as mqtt_client
 import json
 # Set GPIO mode and PIR pin
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 18
+PIR_PIN = 21
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 # Initialize MQTT client
+import logging
+from systemd import journal 
+
+logger = logging.getLogger(__name__)
+logger.addHandler(journal.JournalHandler())
+logger.setLevel(logging.DEBUG)
+
 
 mqtt_topic_base = "rpi_pir"
 mqtt_topic_availability = mqtt_topic_base + "/status"
@@ -16,60 +23,62 @@ mqtt_topic_motion = mqtt_topic_base + "/motion"
 
 class MQTT():
     def __init__(self):
-        self.mqtt_broker = '127.0.0.1'
+        self.mqtt_broker = 'localhost'
         self.mqtt_port = 8883
-        self.mqtt_client_id = "rpi_pir_sensor"
+        self.mqtt_client_id = "rpi_cam_sensor"
         self.username = 'rami'
         self.password = '5461'
         self.mqtt_client = mqtt_client.Client(self.mqtt_client_id)
         self.RECONNECT_RATE = 1.1
         self.MAX_RECONNECT_COUNT = 5
-        self.MAX_RECONNECT_DELAY = 15
         self.reconnect_delay = 2
-        self.disconnectFalg = False
-    def mqtt_on_connect(self, client, userdata, flags, rc):
-        print("Connected to MQTT Broker YAAAAAAAAAAAAAAAAAY!")
+        self.MAX_RECONNECT_DELAY = 10
+        self.DisconnectFlag = False
+    def mqtt_on_connect(self , client, userdata, flags, rc):
+        logger.info("Connected to MQTT Broker YAAAAAAAAAAAAAAAAAY!")
         if rc == 0:
-            print("Connected to MQTT Broker YAAAAAAAAAAAAAAAAAY!")
+            logger.info("Connected to MQTT Broker YAAAAAAAAAAAAAAAAAY!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            logger.info("Failed to connect, return code %d\n", rc)
 
     def mqtt_on_disconnect(self ,client, userdata, rc):
-        if (self.disconnectFalg):
+        if (self.DisconnectFlag == True ):
             return
         print("Disconnected with result code: %s", rc)
         reconnect_count = 0
         while reconnect_count < self.MAX_RECONNECT_COUNT:
-            print("Reconnecting in %d seconds...", self.reconnect_delay)
+            logger.info("Reconnecting in %d seconds...", self.reconnect_delay)
             time.sleep(self.reconnect_delay)
             try:
                 client.reconnect()
-                print("Reconnected successfully!")
+                logger.info("Reconnected successfully!")
+                client.publish(mqtt_topic_availability,"online", retain=True)
                 return
             except Exception as err:
-                print("%s. Reconnect failed. Retrying...", err)
+                logger.error("%s. Reconnect failed. Retrying...", err)
             self.reconnect_delay *= self.RECONNECT_RATE
             self.reconnect_delay = min(self.reconnect_delay, self.MAX_RECONNECT_DELAY)
             reconnect_count += 1
-        print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+        logger.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
 
     def connect_mqtt(self):
-        print("connect_mqtt")
+        logger.info("connect_mqtt")
         # Set Connecting Client ID
         self.mqtt_client = mqtt_client.Client(self.mqtt_client_id)
         self.mqtt_client.username_pw_set(self.username, self.password)
         self.mqtt_client.on_connect = self.mqtt_on_connect
         self.mqtt_client.on_disconnect = self.mqtt_on_disconnect
         self.mqtt_client.connect(self.mqtt_broker, self.mqtt_port)
-        self.mqtt_client.will_set(mqtt_topic_availability, payload="offline", qos=1, retain=True)
-        print("connect_mqtt done")
+        #self.mqtt_client.will_set(mqtt_topic_availability, payload="offline", qos=1, retain=True)
+        logger.info("connect_mqtt done")
         return self.mqtt_client
+
     
 MqttClass =  MQTT()
 _mqtt_client = MqttClass.connect_mqtt()
 ExitAllThread = False
 globalbusy = False
-_mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"0"}))
+_mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"off"}))
 _mqtt_client.publish(mqtt_topic_availability,"online", retain=True)
 
 
@@ -79,9 +88,10 @@ def motion_detected(channel):
     global _mqtt_client 
     if GPIO.input(PIR_PIN):
         print("Motion Detected!")
-        _mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"1"}))
+        _mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"on"}))
     else : 
-        _mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"0"}))
+        print("Motion NOT Detected!")
+        _mqtt_client.publish(mqtt_topic_motion,json.dumps({"motion" :"off"}))
 
 
 # Set up GPIO event detection
