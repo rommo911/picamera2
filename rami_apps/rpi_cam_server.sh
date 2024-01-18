c04e5de86a21cd6a1abce40caae95b44deb858e6
Rami ZAYAT <rami.zayat9@gmail.com>
2024-01-18 23:25:24 +0100

    fixed pyhton version in script

--- rami_apps/rpi_cam_server.sh
+++ rami_apps/rpi_cam_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python3.9 -u /home/rpirami/camerpay2python/picamera2/rami_apps/Rami_cam_services.py
+/usr/bin/python -u /home/rpirami/camerpay2python/picamera2/rami_apps/Rami_cam_services.py
